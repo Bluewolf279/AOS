120c5c418b54620228470ca7cbc65b578d186c13
Fariko-Dungeonz <theextr3megamer@gmail.com>
2015-10-10 19:57:30 +1000

    Test World Creation + Basic item render + Asset correct

--- src/java/com/TRIUMPH/AOS/ClientProxy.java
+++ src/java/com/TRIUMPH/AOS/ClientProxy.java
@@ -1,5 +1,7 @@
 package com.TRIUMPH.AOS;
 
+import com.TRIUMPH.AOS.client.render.items.ItemRenderRegister;
+
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
@@ -13,6 +15,8 @@ public class ClientProxy extends CommonProxy {
 	@Override
 	public void init(FMLInitializationEvent e) {
 		super.init(e);
+		
+		ItemRenderRegister.registerItemRenderer();
 	}
 	@Override
 	public void postInit(FMLPostInitializationEvent e) {
--- src/java/com/TRIUMPH/AOS/items/BasicItem.java
+++ src/java/com/TRIUMPH/AOS/items/BasicItem.java
@@ -1,6 +1,7 @@
 package com.TRIUMPH.AOS.items;
 
 import net.minecraft.item.Item;
+import net.minecraft.creativetab.*;
 
 public class BasicItem extends Item{
 	
