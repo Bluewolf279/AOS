d076b158cf599e552cc796e119a3e7b23126f9c9
Fariko-Dungeonz <theextr3megamer@gmail.com>
2015-10-10 17:38:12 +1000

    Foundation Work - Basic items

--- src/java/com/TRIUMPH/AOS/items/ModItems.java
+++ src/java/com/TRIUMPH/AOS/items/ModItems.java
@@ -5,6 +5,6 @@ import net.minecraft.item.Item;
 public class ModItems {
 	public static Item tutorialItem;
 	public static void createItem() {
-		
+		GameRegistry.registerItem(tutorialItem = new BasicItem("tutorial_item"), "tutorial_item");
 	}
 }
