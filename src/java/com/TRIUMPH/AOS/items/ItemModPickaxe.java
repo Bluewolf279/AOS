62a8a8db0573fd8351b17d120fa92710c1869a40
Bluewolf <Tracymatt2@hotmail.co.uk>
2015-12-21 18:54:21 +1100

    Added various FIles

--- src/java/com/TRIUMPH/AOS/items/ItemModPickaxe.java
+++ src/java/com/TRIUMPH/AOS/items/ItemModPickaxe.java
@@ -4,6 +4,7 @@ import net.minecraft.item.ItemPickaxe;
 
 /**
  * Created by Bluewolf on 21/12/2015.
+ * This is a pickaxe constructor
  */
 public class ItemModPickaxe extends ItemPickaxe{
     public ItemModPickaxe(String unlocalisedName, ToolMaterial Material){
