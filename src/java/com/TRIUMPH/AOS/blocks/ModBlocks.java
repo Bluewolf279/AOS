b58621d109ef4a9922690a4937198dec5e2c4d65
Bluewolf <Tracymatt2@hotmail.co.uk>
2015-12-22 21:33:14 +1100

    Changed how Unlocalized names are defined

--- src/java/com/TRIUMPH/AOS/blocks/ModBlocks.java
+++ src/java/com/TRIUMPH/AOS/blocks/ModBlocks.java
@@ -1,10 +1,11 @@
 package com.TRIUMPH.AOS.blocks;
 
+import com.TRIUMPH.AOS.unlocalizedNames;
 import net.minecraft.block.Block;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class ModBlocks {
-	
+
 	public static Block BlueGlass;
 	public static Block EnderNuggetOre;
 	public static Block GreenStone;
@@ -13,12 +14,12 @@ public class ModBlocks {
 	public static Block roughGreenstone;
 	public static Block EnderGreenGlass;
 	public static void createBlock(){
-		GameRegistry.registerBlock(BlueGlass = new BasicGlass("BlueGlass"), "BlueGlass");
-		GameRegistry.registerBlock(EnderNuggetOre = new BasicBlock("EnderNuggetOre"), "EnderNuggetOre");
-		GameRegistry.registerBlock(GreenStone = new BasicBlock("GreenStone"), "GreenStone");
-		GameRegistry.registerBlock(smGreenstone = new BasicBlock("smGreenstone"), "smGreenstone");
-		GameRegistry.registerBlock(GreenStone_Brick = new BasicBlock("GreenStone_Brick"), "GreenStone_Brick");
-		GameRegistry.registerBlock(roughGreenstone = new BasicBlock("roughGreenstone"), "roughGreenstone");
-		GameRegistry.registerBlock(EnderGreenGlass = new BasicGlass("EnderGreenGlass"), "EnderGreenGlass");
+		GameRegistry.registerBlock(BlueGlass = new BasicGlass(unlocalizedNames.BlueGlass), unlocalizedNames.BlueGlass);
+		GameRegistry.registerBlock(EnderNuggetOre = new BasicBlock(unlocalizedNames.EnderCrystalOre), unlocalizedNames.EnderCrystalOre);
+		GameRegistry.registerBlock(GreenStone = new BasicBlock(unlocalizedNames.GreenStone), unlocalizedNames.GreenStone);
+		GameRegistry.registerBlock(smGreenstone = new BasicBlock(unlocalizedNames.SmoothGreenStone), unlocalizedNames.SmoothGreenStone);
+		GameRegistry.registerBlock(GreenStone_Brick = new BasicBlock(unlocalizedNames.BrickGreenStone), unlocalizedNames.BrickGreenStone);
+		GameRegistry.registerBlock(roughGreenstone = new BasicBlock(unlocalizedNames.RoughGreenStone), unlocalizedNames.RoughGreenStone);
+		GameRegistry.registerBlock(EnderGreenGlass = new BasicGlass(unlocalizedNames.EnderGreenGlass), unlocalizedNames.EnderGreenGlass);
 	}
 }
--- src/java/com/TRIUMPH/AOS/items/ModItems.java
+++ src/java/com/TRIUMPH/AOS/items/ModItems.java
@@ -1,5 +1,6 @@
 package com.TRIUMPH.AOS.items;
 
+import com.TRIUMPH.AOS.unlocalizedNames;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 import net.minecraft.item.Item.ToolMaterial;
@@ -28,17 +29,17 @@ public class ModItems {
 	public static Item Ender_CrystalBoots;
 
 	public static ToolMaterial EndercrystalTo = EnumHelper.addToolMaterial("Ender_crystalTo", 3, 2000, 10.0F, 3.0F, 15);
-	public static ArmorMaterial EndercrystalAr = EnumHelper.addArmorMaterial("Ender_crystal","AOS:EndercrystalAr",33,new int[]{3,8,6,3},15);
+	public static ArmorMaterial EndercrystalAr = EnumHelper.addArmorMaterial(unlocalizedNames.EnderCrystal,"AOS:EndercrystalAr",33,new int[]{3,8,6,3},15);
 	public static void createItem() {
 		GameRegistry.registerItem(Poison_Arrow	= new BasicItem("Poison Arrow"), "Poison Arrow");
-		GameRegistry.registerItem(Ender_nugget	= new BasicItem("Ender_nugget"), "Ender_nugget");
-		GameRegistry.registerItem(Ender_crystal	= new BasicItem("Ender_crystal"), "Ender_crystal");
-		GameRegistry.registerItem(Ender_crystalSword = new ItemModSword("Ender_crystalSword",EndercrystalTo), "Ender_crystalSword");
-		GameRegistry.registerItem(Ender_CrystalPickaxe = new ItemModPickaxe("Ender_CrystalPickaxe",EndercrystalTo), "Ender_CrystalPickaxe");
+		GameRegistry.registerItem(Ender_nugget	= new BasicItem(unlocalizedNames.EnderNugget), unlocalizedNames.EnderNugget);
+		GameRegistry.registerItem(Ender_crystal	= new BasicItem(unlocalizedNames.EnderCrystal), unlocalizedNames.EnderCrystal);
+		GameRegistry.registerItem(Ender_crystalSword = new ItemModSword(unlocalizedNames.EnderCrystalSword,EndercrystalTo), unlocalizedNames.EnderCrystalSword);
+		GameRegistry.registerItem(Ender_CrystalPickaxe = new ItemModPickaxe(unlocalizedNames.EnderCrystalPickaxe,EndercrystalTo), unlocalizedNames.EnderCrystalPickaxe);
 
-		GameRegistry.registerItem(Ender_CrystalHelmet = new ItemModArmor("Ender_CrystalHelmet",EndercrystalAr,1,0),"Ender_CrystalHelmet");
-		GameRegistry.registerItem(Ender_CrystalChestpiece = new ItemModArmor("Ender_CrystalChestpiece",EndercrystalAr,1,1),"Ender_CrystalChestpiece");
-		GameRegistry.registerItem(Ender_CrystalLeggings = new ItemModArmor("Ender_CrystalLeggings",EndercrystalAr,2,2),"Ender_CrystalLeggings");
-		GameRegistry.registerItem(Ender_CrystalBoots = new ItemModArmor("Ender_CrystalBoots",EndercrystalAr,1,3),"Ender_CrystalBoots");
+		GameRegistry.registerItem(Ender_CrystalHelmet = new ItemModArmor(unlocalizedNames.EnderCrystalHelmet,EndercrystalAr,1,0),unlocalizedNames.EnderCrystalHelmet);
+		GameRegistry.registerItem(Ender_CrystalChestpiece = new ItemModArmor(unlocalizedNames.EnderCrystalChestplate,EndercrystalAr,1,1),unlocalizedNames.EnderCrystalChestplate);
+		GameRegistry.registerItem(Ender_CrystalLeggings = new ItemModArmor(unlocalizedNames.EnderCrystalLeggings,EndercrystalAr,2,2),unlocalizedNames.EnderCrystalLeggings);
+		GameRegistry.registerItem(Ender_CrystalBoots = new ItemModArmor(unlocalizedNames.EnderCrystalBoots,EndercrystalAr,1,3),unlocalizedNames.EnderCrystalBoots);
 	}
 }
